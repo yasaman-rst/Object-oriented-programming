17cf4fe66613d85bd09bc8620e3a8feed1f2b0f9
yasaman-rst <yasaman.rostami@edu.turkuamk.fi>
2025-02-05 14:58:16 +0200

    Pushing Ex2No6

---
+++ Lecture 2/Ex2No6.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, name, author, genre, year):
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.year = year
+
+python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+
+print(f"{python.author}: {python.name} ({python.year})")
+print(f"The genre of the book {everest.name} is {everest.genre}")
